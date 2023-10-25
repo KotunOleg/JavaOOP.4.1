bee6313d16dc92e2f1ffa007b9d6fc31a529dab0
KotunOleg <147324130+KotunOleg@users.noreply.github.com>
2023-10-26 01:52:00 +0300

    Update Constructor.java

--- Constructor.java
+++ Constructor.java
@@ -1,5 +1,1 @@
-public class Constructor {
-Student st1 = new Student("Vasya", "Pupkin", Gender.MALE, 1,  "group");
-Student st2 = new Student("Vasilisa", "Chudesnaya", Gender.FEMALE, 2, "Group of students");
 
-}
